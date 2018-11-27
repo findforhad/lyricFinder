860cef8687b6b89b8e2ae20fb02ac33be8f6025a
MacBook Air <hello@MacBooks-MacBook-Air.local>
2018-11-28 05:38:39 +0600

    Updates

---
+++ precache-manifest.02d4ab1c701f08665dbcc38deec0af26.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fef1f20aa76540808c4f5b004bfe49bd",
+    "url": "/lyricfinder/static/media/spinner.fef1f20a.gif"
+  },
+  {
+    "revision": "404cd28e19c4c9466cf3",
+    "url": "/lyricfinder/static/js/runtime~main.404cd28e.js"
+  },
+  {
+    "revision": "8a6ff78ce2ccf67111b3",
+    "url": "/lyricfinder/static/js/main.8a6ff78c.chunk.js"
+  },
+  {
+    "revision": "12fbe4b81e189d5526e7",
+    "url": "/lyricfinder/static/js/1.12fbe4b8.chunk.js"
+  },
+  {
+    "revision": "bfa0e679ff2af2460e18e75744870e46",
+    "url": "/lyricfinder/index.html"
+  }
+];
